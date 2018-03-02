ea834f68217aa2324d456300c915177c0649b861
win <soulfaster@hotmail.com>
2018-03-02 11:57:12 +0700

    Authen access

--- app/src/main/java/com/example/soulf/mushroomiotfarm/fragment/AuthenFragment.java
+++ app/src/main/java/com/example/soulf/mushroomiotfarm/fragment/AuthenFragment.java
@@ -1,6 +1,7 @@
 package com.example.soulf.mushroomiotfarm.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,6 +12,10 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.soulf.mushroomiotfarm.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * Created by soulf on 3/2/2018.
@@ -45,11 +50,31 @@ public class AuthenFragment extends Fragment {
                     Toast.makeText(getActivity(), "Please Fill All Blank", Toast.LENGTH_SHORT).show();
                 } else {
 //                    No Space
-
+                    checkAuthen(userString, passwordString);
                 }
             }
         });
     }
+
+    private void checkAuthen(String userString, String passwordString) {
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInWithEmailAndPassword(userString, passwordString)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+//                            Success Authen
+                            Toast.makeText(getActivity(), "Welcome", Toast.LENGTH_SHORT).show();
+                            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.contentMainFragment, new MainFragment()).commit();
+                        } else {
+//                            UnSuccess Authen
+                            Toast.makeText(getActivity(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    } //checkAuthen
 
     @Nullable
     @Override
