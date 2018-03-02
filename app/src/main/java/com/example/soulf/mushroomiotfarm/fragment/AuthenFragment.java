6c2f0e1ad1cf0b556b80ea43ad96501ac767a311
win <soulfaster@hotmail.com>
2018-03-02 11:21:19 +0700

    Check space ok

--- app/src/main/java/com/example/soulf/mushroomiotfarm/fragment/AuthenFragment.java
+++ app/src/main/java/com/example/soulf/mushroomiotfarm/fragment/AuthenFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.soulf.mushroomiotfarm.R;
 
@@ -14,6 +17,40 @@ import com.example.soulf.mushroomiotfarm.R;
  */
 
 public class AuthenFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Login Controller
+        loginController();
+
+    }  //Main Method
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                EditText userEditText = getView().findViewById(R.id.editUser);
+                EditText PasswordEdiText = getView().findViewById(R.id.editPassword);
+
+                String userString = userEditText.getText().toString().trim();
+                String passwordString = PasswordEdiText.getText().toString().trim();
+
+                if (userString.isEmpty() || passwordString.isEmpty()) {
+//                    Have space
+                    Toast.makeText(getActivity(), "Please Fill All Blank", Toast.LENGTH_SHORT).show();
+                } else {
+//                    No Space
+
+                }
+            }
+        });
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
